317f8a39803f716f87e82edfb2bf4069482a7b01
Rakesh Mishra <62935631+rakeshakki2020@users.noreply.github.com>
2021-09-08 23:41:49 +0530

    Update unity.c

--- 3.Implementation of project/Unity/unity.c
+++ 3.Implementation of project/Unity/unity.c
@@ -2109,15 +2109,4 @@ int UnityTestMatches(void)
 
 #endif /* UNITY_USE_COMMAND_LINE_ARGS */
 /*-----------------------------------------------*/
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
